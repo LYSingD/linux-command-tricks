be8337d77d97c065e7f905edc38ad08803ed0897
LYSingD <me@davidlys.dev>
2023-01-05 00:24:58 -0500

    Add tricks for create, empty, truncate file

---
+++ create-empty-truncate-file.sh
@@ -0,0 +1,4 @@
+# The desination file is always rewritten from the beginning when we redirect output with the '>' redirection operator. 
+# Simply using the redirection operator with no command preceding it will truncate an existing file or create a new, empty file. 
+
+> your-example-file.txt
